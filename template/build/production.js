23ec9f21f9acc554e225b279b7fe505a71e9f946
John Lioneil Dionisio <john.dionisio1@gmail.com>
2017-09-21 11:58:50 +0800

    !:)

--- template/build/production.js
+++ template/build/production.js
@@ -4,7 +4,7 @@ function install (Vue) {
   Vue.component('{{name}}', {{uppercase name}});
 };
 
-export default = install
+export default install
 
 if (typeof window !== 'undefined' && window.Vue) {
   Vue.use(install)
